80fb4655b663bf8a831b015997bbb85d5e36eec3
Milan Poria <142563751+Milan-imca@users.noreply.github.com>
2024-04-24 14:48:36 +0530

    Update Task.jsx

--- TaskManager(Task2)/task-manager-app/src/components/Task/Task.jsx
+++ TaskManager(Task2)/task-manager-app/src/components/Task/Task.jsx
@@ -15,12 +15,12 @@ const Task = ({ task, onDelete, onToggleComplete, onEdit }) => {
   };
 
   return (
-    <div className='border-2  rounded-lg p-3 m-2 shadow-xl w-[300px] flex flex-col justify-center hover:shadow-2xl'>
-      <div className='p-3'>
+    <div className=' bg-white-100 border-none  rounded-lg p-3 m-2 shadow-xl w-[300px] flex flex-col justify-center hover:shadow-2xl'>
+      <div className='p-3 '>
         <span className='text-right text-lg font-medium'>
           {task.text}
         </span>
-        <div className='mt-5 text-sm '>Created: {new Date(task.createdDate).toLocaleString()}</div>
+        <div className='mt-5 text-sm  '>Created: {new Date(task.createdDate).toLocaleString()}</div>
         {task.completed && (
           <div className=' text-sm '>
             Completed: {new Date(task.completedDate).toLocaleString()}
@@ -29,12 +29,12 @@ const Task = ({ task, onDelete, onToggleComplete, onEdit }) => {
       </div>
       <div className=' flex justify-end '>
         {!task.completed && (
-          <button className='bg-red-300 p-1 rounded-md m-1  pt-1 pb-1 pr-2 pl-2 font-medium hover:bg-red-100 hover:text-white' onClick={() => onToggleComplete(task.id)}>
+          <button className='bg-green-300 p-1 rounded-md m-1  pt-1 pb-1 pr-2 pl-2 font-medium hover:bg-green-200 hover:text-white' onClick={() => onToggleComplete(task.id)}>
             Mark Complete
           </button>
         )}
         {!task.completed && <button className="bg-purple-500 pt-1 pb-1 pr-2 pl-2 rounded-md m-1 font-medium hover:bg-purple-300 hover:text-white" onClick={handleEdit}>Edit</button>}
-        <button className='bg-green-400 p-1 rounded-md  m-1  pt-1 pb-1 pr-2 pl-2 font-medium hover:bg-green-300 hover:text-white' onClick={() => onDelete(task.id)}>Delete</button>
+        <button className='bg-red-400 p-1 rounded-md  m-1  pt-1 pb-1 pr-2 pl-2 font-medium hover:bg-red-300 hover:text-white' onClick={() => onDelete(task.id)}>Delete</button>
       </div>
       {isEditing && (
         <div className=' p-3'>
