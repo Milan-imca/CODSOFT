d7ee9626edd66236321ebbfdc6e867427e39e5bd
Milan Poria <142563751+Milan-imca@users.noreply.github.com>
2024-04-24 14:46:58 +0530

    Update TaskForm.jsx

--- TaskManager(Task2)/task-manager-app/src/components/Task/TaskForm.jsx
+++ TaskManager(Task2)/task-manager-app/src/components/Task/TaskForm.jsx
@@ -14,7 +14,7 @@ const TaskForm = ({ onSubmit }) => {
     <div className=' p-4 flex justify-center'>
       <form onSubmit={handleSubmit}>
         <input type="text" value={text} onChange={(e) => setText(e.target.value)} className='p-2 rounded-md shadow-lg' placeholder='Enter task' />
-        <button type="submit" className='bg-green-400 p-2 ml-2 font-semibold text-white rounded-md text-md shadow-md hover:bg-green-200'>Add Task</button>
+        <button type="submit" className='bg-blue-400 p-2 ml-2 font-semibold text-white rounded-md text-md shadow-xl hover:bg-blue-200 hover:shadow-lg'>Add Task</button>
       </form>
     </div>
   );
